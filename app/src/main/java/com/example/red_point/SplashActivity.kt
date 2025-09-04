8542e320d69ff296dbfc89613d0755e438e299c0
Diego-lion <dlmonteroortiz13@gmail.com>
2025-09-04 00:09:11 -0500

    NEW REDPOINTT

--- app/src/main/java/com/example/red_point/SplashActivity.kt
+++ app/src/main/java/com/example/red_point/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.red_point
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-             setContentView(R.layout.activity_splash)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        android.os.Handler().postDelayed({
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
     }
+
 }
